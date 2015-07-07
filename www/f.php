e0d608a4b99a80dc3f18b6483bbc0ab95e163a60
Cris Stanza <crisstanza@home>
2015-07-07 00:01:00 -0300

    "F" framework and initial service code.

---
+++ www/f.php
@@ -0,0 +1,33 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+class F { 
+
+	public static function setFromRequest($instance) {
+		$class_name = get_class($instance);
+		$members = get_class_vars($class_name);
+		foreach ($members as $name => $value) {
+			$parameterValue = isset($_REQUEST[$name]) ? urldecode($_REQUEST[$name]) : '';
+			$parameterValueParsed = Base::parse($class_name, $name, $parameterValue);
+			$instance->{$name} = $parameterValueParsed;
+		}
+		return $instance;
+	}
+
+	public static function getRequestBody() {
+		$rawInput = fopen('php://input', 'r');
+		$tempStream = fopen('php://temp', 'r+');
+		stream_copy_to_stream($rawInput, $tempStream);
+		rewind($tempStream);
+		return stream_get_contents($tempStream);
+	}
+
+	public static function isPost() {
+		$method = $_SERVER['REQUEST_METHOD'];
+		return $method == 'POST';
+	}
+
+}
+
+?>
--- www/service.php
+++ www/service.php
@@ -1,2 +1,12 @@
-         
+<?php
+
+include_once('./f.php');
+
+if (F::isPost()) {
+
+} else {
+	
+}
+
+?>         
 Cris Stanza
