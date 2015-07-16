80604941f353a1d5c5add50e2b0297a1b263951c
Cris Stanza <crisstanza@work>
2015-07-16 11:11:19 -0300

    DAO implementation

--- www/f.classes.php
+++ www/f.classes.php
@@ -2,8 +2,27 @@
 
 class Game {
 
+	/** type=string */
+	var $board;
+
+	/** type=numeric&id=true */
+	var $id;
+
+	/** type=numeric */
+	var $status;
+
+	/** type=string */
+	var $turn;
+
+	public function Game($id) {
+		$this->id = $id;
+	}
+
 }
 
+//////////////////////////////////////////////////////////////////////
+
+/** operation */
 class GetGame {
 
 }
@@ -11,14 +30,18 @@ class GetGame {
 class GetGameOperator {
 
 	public function operate($operation, $dao) {
+		var $game = $dao::findGameById(1);
 		echo '         '."\n".'0'."\n".'Cris Stanza';
+		echo F::getFromInstance($game);
 	}
 
 }
 
-class GetGameDao {
+class GetGameDao extends D {
 	
+	public function findGameById($id) {
+		return base::findById(new Game($id));
+	}
 }
-
 
 ?>
--- www/f.php
+++ www/f.php
@@ -6,6 +6,12 @@
 
 date_default_timezone_set('UTC');
 
+/*
+	type=numeric|string|bool
+	mandatory=true|false
+	id=true|false
+	transient=true|false
+*/
 class ServiceRequest {
 
 	/** type=string&mandatory=false */
@@ -13,7 +19,7 @@ class ServiceRequest {
 
 }
 
-abstract class F { 
+abstract class F {
 
 	public static function setFromStringName($class_name) {
 		return new $class_name;
@@ -24,7 +30,7 @@ abstract class F {
 		$members = get_class_vars($class_name);
 		foreach ($members as $name => $value) {
 			$parameterValue = isset($_REQUEST[$name]) ? urldecode($_REQUEST[$name]) : '';
-			$parameterValueParsed = F::parse($class_name, $name, $parameterValue);
+			$parameterValueParsed = I::parse($class_name, $name, $parameterValue);
 			$instance->{$name} = $parameterValueParsed;
 		}
 		return $instance;
@@ -33,7 +39,7 @@ abstract class F {
 	public static function setFromRequestBody($instance) {
 		$class_name = get_class($instance);
 		$members = get_class_vars($class_name);
-		$requestBody = F::getRequestBody();
+		$requestBody = self::getRequestBody();
 		$lines = explode("\n", $requestBody);
 		$i = 0;
 		foreach ($members as $name => $value) {
@@ -44,34 +50,15 @@ abstract class F {
 		return $instance;
 	}
 
-	public static function parse($class_name, $property, $value) {
-		$type = F::type($class_name, $property);
-		switch ($type) {
-    		case 'bool':
-    			return F::booleanValue($value);
-        	break;
-			default:
-				return $value;
-        	break;
+	public static function getFromInstance($instance) {
+		$class_name = get_class($instance);
+		$members = get_class_vars($class_name);
+		$result = array();
+		foreach ($members as $name => $value) {
+			$lineValue = urlencode($value);
+			array_push($result, $lineValue, "\n");
 		}
-	}
-
-	public static function type($class_name, $property) {
-		return F::annotationValue($class_name, $property, 'type');
-	}
-
-	public static function booleanValue($value) {
-		return strtolower($value) == 'true' || $value == 1 ? '1': '0';
-	}
-
-	public static function annotationValue($class_name, $property, $annotation_name) {
-		$rc = new ReflectionClass($class_name);
-		$comment = $rc->getProperty($property)->getDocComment();
-		$start = strpos($comment, '/**') + 3;
-		$end = strpos($comment, '*/') - 3;
-		$annotationString = trim(substr($comment, $start, $end));
-		parse_str($annotationString, $annotation);
-		return isset($annotation[$annotation_name]) ? $annotation[$annotation_name] : '';
+		return join($result);
 	}
 
 	public static function getRequestBody() {
@@ -86,6 +73,111 @@ abstract class F {
 	public static function isPost() {
 		$method = $_SERVER['REQUEST_METHOD'];
 		return $method == 'POST';
+	}
+
+}
+
+abstract class I {
+
+	public static function annotationValue($class_name, $property, $annotation_name) {
+		$rc = new ReflectionClass($class_name);
+		$comment = $rc->getProperty($property)->getDocComment();
+		$start = strpos($comment, '/**') + 3;
+		$end = strpos($comment, '*/') - 3;
+		$annotationString = trim(substr($comment, $start, $end));
+		parse_str($annotationString, $annotation);
+		return isset($annotation[$annotation_name]) ? $annotation[$annotation_name] : '';
+	}
+
+	public static function parse($class_name, $property, $value) {
+		$type = self::type($class_name, $property);
+		switch ($type) {
+    		case 'bool':
+    			return N::booleanValue($value);
+        	break;
+			default:
+				return $value;
+        	break;
+		}
+	}
+
+	public static function type($class_name, $property) {
+		return self::annotationValue($class_name, $property, 'type');
+	}
+
+}
+
+abstract class N {
+
+	public static function booleanValue($value) {
+		return strtolower($value) == 'true' || $value == 1 ? '1': '0';
+	}
+
+}
+
+abstract class D {
+
+	public function open() {
+		@$con = mysql_connect('localhost', 'user', 'pass');
+		if (!$con) {
+			throw new Exception(mysql_errno());
+		}
+		@$db = mysql_select_db('base'), $con);
+		if (!$db) {
+			throw new Exception(mysql_errno());
+		}
+		return $con;
+	}
+
+	public function close($con, $conWasNull) {
+		if ($conWasNull) {
+			@$rs = mysql_close($con);
+			if (!$rs) {
+				throw new Exception(mysql_errno().' '.mysql_error());
+			}
+		}
+	}
+
+	public static function quotes($class_name, $property, $str, $con) {
+		$type = I::type($class_name, $property);
+		if (strlen(trim($str)) <= 0) {
+			return "NULL";
+		} else if (is_numeric($str) || is_bool($str)){
+			return $str;
+		} else {
+			return "'".mysql_real_escape_string($str, $con)."'";
+		}
+	}
+
+	private function select($sql, $con) {
+		@$rs = mysql_query($sql, $con);
+		if (!$rs) {
+			if ($con == null) {
+				throw new Exception('$con == null');
+			} else {
+				throw new Exception(mysql_errno().' '.mysql_error());
+			}
+		}
+		return $rs;
+	}
+
+	public static function sqlFindById($con, $obj) {
+		$class_name = get_class($obj);
+		$sql = array();
+		array_push($sql, 'SELECT * FROM ', strtolower($class_name), ' WHERE id=', self::quotes($class_name, 'id', $vo->id, $con));
+		return join($sql);
+	}
+
+	public function findById($obj) {
+		$con = self::open();
+		$sql = self::sqlFindById($con, $obj);
+		$rs = self::select($sql, $con);
+		$result = null;
+		if($row = mysql_fetch_assoc($rs)) {
+			$result = call_user_func_array(array('self', 'rowToObjectTransformer'), array($row, $con));
+		}
+		self::close($con, true);
+		return $result;
 	}
 
 }
